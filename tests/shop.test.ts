9df5483f2482b37032e894cfa8dcae4f0b9500b4
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2024-03-27 12:26:52 +0200

    more changes

---
+++ tests/shop.test.ts
@@ -0,0 +1,12 @@
+// ➖ Remove this line
+// import { test, expect } from '@playwright/test';
+// ➕ Add this line
+import { test, expect } from "@chromatic-com/playwright";
+
+// Then use as normal 👇
+test("Shop", async ({ page }) => {  
+  await page.goto("https://shopdemo-alex-hot.koyeb.app/shop/brand/Nizhyn");  
+  
+  await expect(page).toHaveTitle("MERN Store");  
+  
+});
