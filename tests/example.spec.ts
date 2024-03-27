0bdf1f6afa0ed70f40c3786326e41d49e85e5ae2
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2024-03-27 12:20:20 +0200

    some change

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,7 +5,7 @@ import { test, expect } from "@chromatic-com/playwright";
 
 // Then use as normal 👇
 test("Homepage", async ({ page }) => {  
-  await page.goto("https://shopdemo-alex-hot.koyeb.app/");  
+  await page.goto("https://shopdemo-alex-hot.koyeb.app/shop/brand/Nizhyn");  
   
   await expect(page).toHaveTitle("MERN Store");  
   
